88307b34887d47ae3e27eefb8824f3ab182fe612
Ibi Hasanli <ibili73@gmail.com>
2021-12-20 21:21:18 +0000

    Initial COmmit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AuthPage from './AuthPage';
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <AuthPage /> */}
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
