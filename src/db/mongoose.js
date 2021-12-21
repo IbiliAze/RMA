72206f4b7a74ce6b20e6f5060ff118c98ecf4a18
Ibi Hasanli <ibili73@gmail.com>
2021-12-21 23:38:55 +0000

    mongodb

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 //////////////////////////////////////////////////////////////////////////////////////////
 
-const url = process.env.MONGODB_URL;
-const dbName = process.env.MONGODB_NAME || 'RMA';
-const connectionURL = `mongodb://${url}:${process.env.MONGODB_PORT}/${dbName}`;
+const connectionURL = process.env.MONGODB_URL;
 
 console.log(`Connecting to ${connectionURL}`);
 mongoose.connect(
@@ -20,10 +18,6 @@ mongoose.connect(
     if (error) {
       return console.log(`Connection error: ${error}`);
     }
-    console.log(
-      `${chalk.bold.green('Successful')} MongoDB Connection: [ ${chalk.bold.blue(dbName)} ] at [ ${chalk.bold.blue(
-        url
-      )}:${chalk.bold.blue(process.env.MONGODB_PORT)} ]`
-    );
+    console.log(`${chalk.bold.green('Successful')} MongoDB Connection: at [ ${chalk.bold.blue(connectionURL)} ]`);
   }
 );
